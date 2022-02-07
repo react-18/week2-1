120a367a57597c587ab77aa47fe65a6b3953fe48
rachel490 <ccori12@naver.com>
2022-02-07 19:26:29 +0900

    Chore: 불필요한 파일 제거 및 ESLint, Prettier 설정

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "airbnb",
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended",
+        'plugin:prettier/recommended'
+
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    "rules": {
+        "react/jsx-filename-extension" : 0,
+        "import/extensions": 0,
+        "import/no-unresolved": 0,
+        "comma-dangle": 0,
+        "no-shadow": 0,
+    }
+}
