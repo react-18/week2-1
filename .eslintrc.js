7fc48e8651879227bc9c73e3227073abee165c32
gy <gayunlee11@gmail.com>
2022-02-07 21:27:46 +0900

    Feat: Sidebar 컴포넌트 추가
    Style: css 수정
    
    Feat: 메뉴 버튼 컴포넌트 생성
    
    Feat: 사이드바 스타일 컴포넌트 props 추가
    
    Feat: 토글 버튼 클릭시 사이드바 슬라이드 기능 추가
    
    Feat: 영역 밖 클릭 시 슬라이드 닫기 기능 구현
    
    Feat: 사이드바 클릭시 닫기지 않도록 조건 추가
    
    Chore: app.ts 테스트용 코드 삭제
    
    Refactor: 스타일 컴포넌트 속성 지정 순서 변경

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,30 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "airbnb",
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended",
-        'plugin:prettier/recommended'
-
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
-        "react/jsx-filename-extension" : 0,
-        "import/extensions": 0,
-        "import/no-unresolved": 0,
-        "comma-dangle": 0,
-        "no-shadow": 0,
-        "import/prefer-default-export": 0,
-    }
-}
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'react/jsx-filename-extension': 0,
+    'import/extensions': 0,
+    'import/no-unresolved': 0,
+    'comma-dangle': 0,
+    'no-shadow': 0,
+    'import/prefer-default-export': 0,
+  },
+};
