a0144cbaca36e7b38d68ca801dd0f81e13f7d36c
Han-D-Peter <57760806+Han-D-Peter@users.noreply.github.com>
2022-02-08 18:19:00 +0900

    Feat: 셀렉박스, 토글, 카드 컴포넌트 게더링

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,42 @@
+import HeaderNav from 'components/containers/HeaderNav';
+import IsConsult from 'components/containers/IsConsult';
+import RequestCard from 'components/domains/RequestCard';
+import FilterButton from 'components/filter/FilterButton';
+import ResetButton from 'components/filter/ResetButton';
 import React from 'react';
+import { useSelector } from 'react-redux';
+import store from 'store/store';
+
+type RootState = ReturnType<typeof store.getState>;
 
 function App() {
+  const { filteredRequests } = useSelector(
+    (state: RootState) => state.requests,
+  );
   return (
     <div>
-      <h1>Hi, 안녕하세요</h1>
+      <HeaderNav />
+      <div>
+        <div>
+          <FilterButton name="가공방식" options={['밀링', '선반']} />
+          <FilterButton
+            name="재료"
+            options={['알루미늄', '탄소강', '구리', '합금강', '강철']}
+          />
+          <ResetButton />
+        </div>
+        <div>
+          <IsConsult />
+        </div>
+      </div>
+      <div>
+        {filteredRequests.map((filteredRequest) => (
+          <RequestCard
+            key={filteredRequest.id}
+            initialState={filteredRequest}
+          />
+        ))}
+      </div>
     </div>
   );
 }
