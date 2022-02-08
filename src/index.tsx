9dd69ffd39dbc8f69bab55e4890ee4ca94133966
gy <gayunlee11@gmail.com>
2022-02-08 16:12:24 +0900

    Feat: Sidebar 컴포넌트 추가
    Style: css 수정
    
    Feat: 메뉴 버튼 컴포넌트 생성
    
    Feat: 사이드바 스타일 컴포넌트 props 추가
    
    Feat: 토글 버튼 클릭시 사이드바 슬라이드 기능 추가
    
    Feat: 영역 밖 클릭 시 슬라이드 닫기 기능 구현
    
    Feat: 사이드바 클릭시 닫기지 않도록 조건 추가
    
    Chore: app.ts 테스트용 코드 삭제
    
    Refactor: 스타일 컴포넌트 속성 지정 순서 변경
    
    Chore: 폴더 위치 변경
    
    Fix: 깃 충돌 수정
    
    Chore: 이미지 파일 위치 변경
    
    Fix: app 내용 삭제

--- src/index.tsx
+++ src/index.tsx
@@ -3,18 +3,14 @@ import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from 'styles/global-style';
 import { theme } from 'styles/default-theme';
-import { Provider } from 'react-redux';
 import App from './App';
-import store from './store/store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <App />
-      </ThemeProvider>
-    </Provider>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
